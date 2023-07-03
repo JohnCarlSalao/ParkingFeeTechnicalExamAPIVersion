a535222c192180621fd93acfa6d1bca724f59dc0
johncarlsalao <salaojohn4@gmail.com>
2023-07-03 22:55:33 +0800

    07-03-2023 ProjectStructure

---
+++ base/parkingfee/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Car, ParkingFee
+# Register your models here.
+
+admin.site.register (Car)
+admin.site.register(ParkingFee)
---
+++ base/parkingfee/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+class Car(models.Model):
+    id = models.CharField(max_length=5, primary_key=True)
+    car_brand = models.CharField(max_length=100, null=True)
+    plate_number = models.CharField(max_length=20)
+
+class ParkingFee(models.Model):
+    id = models.CharField(max_length=5, primary_key=True)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    entry_time = models.DateTimeField()
+    exit_time = models.DateTimeField(null=True)
+    total_amount = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+
+
